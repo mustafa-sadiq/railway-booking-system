f10a9c08633ecca4a738017cd26501c89bbe82f4
rexyang36 <49075330+rexyang36@users.noreply.github.com>
2020-12-09 22:03:08 +0800

    some queries

---
+++ samplesql.sql
@@ -0,0 +1,31 @@
+--
+-- this query will show you the schedules going from origin to destination and the cost of that trip
+--
+
+set @origin_id = (select station_id
+		from STATIONS
+		where station_name = "Tomato");
+
+set @dest_id = (select station_id
+		from STATIONS
+		where station_name = "Potato");
+
+select	distinct s.schedule_id, s.transit_name, s.train_id, s.fare + s.fare*  abs(s1.stop_num - s2.stop_num)
+	from	SCHEDULES s,
+		(
+			select t1.scheduled, t1.stop_num
+			from STOPS t1
+			where t1.start_station = @origin_id
+		) as s1,
+		(
+			select t2.scheduled, t2.stop_num
+			from STOPS t2
+			where t2.stop_station = @dest_id
+		) as s2
+	where
+		s.schedule_id = s1.scheduled
+	and	s1.scheduled = s2.scheduled;
+select * from reservation where cust_username = 'parth123';
+/*delete from customers where cust_username='parth123';
+delete from reservation where reservation_num=1; sample deletion*/
+select cust_email from customers,reservation where employee='employee1' and reservation.cust_username=customers.cust_username;
